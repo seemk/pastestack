cf03fc9ee817a4d1ab8c973fc747e8f397887665
seemk <siimkallas@gmail.com>
2014-04-24 18:14:41 +0300

    Added jquery turbolinks

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,15 +1,17 @@
 class SearchesController < ApplicationController
-    before_action :store_location
+    before_action :store_location, only: [:search]
 
     def autocomplete_search
         query = params[:name]
-        pastes = Paste.where{(has_randomized_title == false) & (content =~ "%#{query}%")}
+        pastes = Paste.where{(exposure == 1) & 
+                             (has_randomized_title == false) &
+                             (content =~ "%#{query}%")}
         titles = pastes.map{|p| p.title}
         render json: titles
     end
 
     def search
        query = params[:search_field]
-       @pastes = Paste.where{content =~ "%#{query}%"}
+       @pastes = Paste.where{(exposure == 1) & (content =~ "%#{query}%")}
     end
 end
